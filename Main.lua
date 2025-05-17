7329d5235d52b74d689292f89101173498770707
oklexi <hiimlexilol@gmail.com>
2025-05-16 19:25:34 -0600

    Happy House

---
+++ Main.lua
@@ -0,0 +1,39 @@
+SMODS.Atlas{
+    key = "modicon",   
+    px = 34,
+    py = 34,
+    path = "icon.png"
+}
+
+SMODS.Atlas{
+    key = "Partner",
+    px = 46,
+    py = 58,
+    path = "Partners.png"
+}
+
+-- Happy House
+Partner_API.Partner{
+    key = "happy_house",
+    name = "Happy House Partner",
+    unlocked = true,
+    discovered = true,
+    pos = {x = 0, y = 0},
+    loc_txt = {},
+    atlas = "Partner",
+    config = { extra = { related_card = "j_cry_happyhouse", x_mult = 1.14 } },
+    loc_vars = function(self, info_queue, card)
+        local benefits = 1
+        if next(SMODS.find_card("j_cry_happyhouse")) then benefits = 3.5087719298 end
+        return { vars = {card.ability.extra.x_mult*benefits} }
+    end,
+    calculate = function(self, card, context)
+        if context.partner_main then
+            local benefits = 1
+            if next(SMODS.find_card("j_cry_happyhouse")) then benefits = 3.5087719298 end
+            return {
+                xmult = card.ability.extra.x_mult*benefits,
+            }
+        end
+    end,
+}
---
+++ localization/en-us.lua
@@ -0,0 +1,12 @@
+return {
+    descriptions = {
+        Partner={
+            pnr_crpa_happy_house={
+                name = "Happy House",
+                text = {
+                    "{X:mult,C:white}X#1#{} mult",
+                },
+            },
+        },
+    },
+}
